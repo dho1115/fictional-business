e1ffed979cb86506bc5d9048fb65d9bde2348556
DAVID HO <davidhorph@hotmail.com>
2022-09-12 15:35:05 -0500

    EventEmitter

--- src/app/EventEmitter/child-emitter/child-emitter.component.ts
+++ src/app/EventEmitter/child-emitter/child-emitter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child-emitter',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./child-emitter.component.css']
 })
 export class ChildEmitterComponent implements OnInit {
-
+  @Output() childObject = new EventEmitter<{}>();
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  emitChildObject() {
+    this.childObject.emit({['message from emitChildObject()']: 'Activate displayChildEmitterData inside the parent-emiiter!!!'})
   }
 
 }
--- src/app/EventEmitter/parent-emitter/parent-emitter.component.ts
+++ src/app/EventEmitter/parent-emitter/parent-emitter.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent-emitter.component.css']
 })
 export class ParentEmitterComponent implements OnInit {
-
+  displayChildEmitterDataCalled: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  displayChildEmitterData(fromChildObject: {}) {
+    //fromChildObject will be equal to whatever is inside the ( ) from this.childObject.emit({['message from emitChildObject()']: 'Activate displayChildEmitterData inside the parent-emiiter!!!'}).
+    this.displayChildEmitterDataCalled = !this.displayChildEmitterDataCalled;
+    console.log({ fromChildObject })
   }
 
 }
