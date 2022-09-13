fc63ced844d48dfb566e0a1ae912574fcea276c1
DAVID HO <davidhorph@hotmail.com>
2022-09-13 12:19:57 -0500

    Able to create unique emploee id.

--- src/app/EventEmitter/CompanyDashboard/add-employee/add-employee.component.ts
+++ src/app/EventEmitter/CompanyDashboard/add-employee/add-employee.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { salesPersonModel } from '../Models/salesPersonModel.model';
 @Component({
   selector: 'app-add-employee',
   templateUrl: './add-employee.component.html',
   styleUrls: ['./add-employee.component.css']
 })
 export class AddEmployeeComponent implements OnInit {
-  number: number = 0;
-  name: string = ''
-  empID: string = (this.number + 100000) + this.name.split("").join("");
-  
+  number: number = 100000;
+  name: string = '';
+  _empID: string = '';
+  sales: number = 0;
+
+  @Output() salesEmployeeEmitter = new EventEmitter<salesPersonModel>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  emitNewSalesPerson() {
+    ++this.number;
+    this._empID = `${this.number}-${this.name.split("").join("")}`;
+
+    this.salesEmployeeEmitter.emit({_empID: this._empID, name: this.name, sales: this.sales});
+    console.log(`${JSON.stringify({_empID: this._empID, name: this.name, sales: this.sales})} has been emitted!!!`)
+    this.name = ""
+  }
 }
