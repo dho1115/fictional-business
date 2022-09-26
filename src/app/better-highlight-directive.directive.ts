f8ae14d2c59b4b6df8938afc09c20fac59a8e229
DAVID HO <davidhorph@hotmail.com>
2022-09-26 16:46:31 -0500

    app-better-highlight directive.

--- src/app/better-highlight-directive.directive.ts
+++ src/app/better-highlight-directive.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlightDirective]'
 })
-export class BetterHighlightDirectiveDirective {
+export class BetterHighlightDirectiveDirective implements OnInit {
 
-  constructor() { }
+  constructor( private elRef: ElementRef, private renderer: Renderer2 ) { }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow')
+  }
 
 }
