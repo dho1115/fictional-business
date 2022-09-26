85f09ef45a2b52272999ee8cc5d2fd3812874796
DAVID HO <davidhorph@hotmail.com>
2022-09-26 18:39:22 -0500

    figured how to use listen.

--- src/app/better-highlight-directive.directive.ts
+++ src/app/better-highlight-directive.directive.ts
@@ -4,11 +4,12 @@ import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
   selector: '[appBetterHighlightDirective]'
 })
 export class BetterHighlightDirectiveDirective implements OnInit {
-
+  currentColor: string = 'green'
   constructor( private elRef: ElementRef, private renderer: Renderer2 ) { }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow')
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', this.currentColor)
+    this.renderer.listen(this.elRef.nativeElement, 'click', () => this.renderer.setStyle(this.elRef.nativeElement, 'background-color', this.currentColor = this.currentColor == 'green' ? 'red' : 'green'))
   }
 
 }
